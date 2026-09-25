669a8a744100b757c3003cb9fa3bbf12193b7330
Wilton C <131200742+EntropiaEKaos@users.noreply.github.com>
2026-09-25 12:43:47 -0300

    fix(i18n): pass locale to site renderer

--- app/page.tsx
+++ app/page.tsx
@@ -150,7 +150,7 @@ export default async function Home(){
 
   return <PublicSiteChrome settings={localizedSettings} navPages={localizedNavPages} locale={locale}>
     <>
-      <SiteBuilderRenderer sections={sections} settings={localizedSettings} rooms={localizedRooms} promo={localizedPromo} posts={localizedPosts} media={media} whatsappHref={whatsappHref}/>
+      <SiteBuilderRenderer sections={sections} settings={localizedSettings} rooms={localizedRooms} promo={localizedPromo} posts={localizedPosts} media={media} whatsappHref={whatsappHref} locale={locale}/>
       <EventShowcase events={localizedEvents}/>
     </>
   </PublicSiteChrome>;
